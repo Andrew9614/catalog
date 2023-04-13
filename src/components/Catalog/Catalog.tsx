0dc9c6b22b6c048fdc81fec8748b97630c83975a
Andrew <rogaicopyta@example.com>
2023-04-13 17:20:39 +0300

    pagination

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,14 +1,23 @@
-import { getProducts } from '../../redux/catalogReducer';
+import { getProducts, setActivePage } from '../../redux/catalogReducer';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { useEffect } from 'react';
 import { Card } from '../Card/Card';
+import { Pagination } from '@mui/material';
 
 export const Catalog = () => {
   const state = useAppSelector((state) => state.catalog);
   const dispatch = useAppDispatch();
   useEffect(() => {
-    dispatch(getProducts());
+    dispatch(getProducts()).then(() => dispatch(setActivePage(1)));
   }, [dispatch]);
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    dispatch(setActivePage(value));
+  };
+
   return (
     <div>
       {state.isCatalogLoading ? (
@@ -16,6 +25,7 @@ export const Catalog = () => {
       ) : (
         state.products.map((el) => <Card product={el} key={el.id} />)
       )}
+      <Pagination count={state.pageCount} onChange={handlePageChange} />
     </div>
   );
 };
--- src/redux/catalogReducer.ts
+++ src/redux/catalogReducer.ts
@@ -5,25 +5,41 @@ import { catalogAPI } from '../api/api';
 type InitialStateType = {
   isCatalogLoading: boolean;
   products: ProductType[];
+  productsSorted: ProductType[];
+  activePage: number;
+  pageCount: number;
 };
 
 const initialState: InitialStateType = {
   isCatalogLoading: false,
   products: [],
+  productsSorted: [],
+  activePage: 1,
+  pageCount: 0,
 };
+
+const cardPerPage = 6;
 
 export const catalogSlice = createSlice({
   name: 'catalog',
   initialState: initialState,
   reducers: {
-    setProducts: (state, action: PayloadAction<ProductType[]>) => {
-      state.products = action.payload;
+    setActivePage: (state, action: PayloadAction<number>) => {
+      return {
+        ...state,
+        activePage: action.payload,
+        products: state.productsSorted.slice(
+          cardPerPage * (action.payload - 1),
+          cardPerPage * action.payload
+        ),
+      };
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(getProducts.fulfilled, (state, action) => {
-        state.products = action.payload;
+        state.productsSorted = action.payload;
+        state.pageCount = Math.ceil(action.payload.length / cardPerPage);
         state.isCatalogLoading = false;
       })
       .addCase(getProducts.pending, (state) => {
@@ -35,8 +51,7 @@ export const catalogSlice = createSlice({
   },
 });
 
-export const { setProducts } = catalogSlice.actions;
-
+export const { setActivePage } = catalogSlice.actions;
 export const getProducts = createAsyncThunk<ProductType[], void>(
   'catalog/getProducts',
   async () => {
